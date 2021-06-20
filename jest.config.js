6f9d383f85d66318fc45f1f0fe06c57f73a804c0
Wilson Lin <code@wilsonl.in>
2021-06-20 14:49:25 +1000

    Ignore dist from testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/_common.test.ts"],
+  testPathIgnorePatterns: ["/dist/", "/node_modules/", "/_common.test.ts"],
 };
